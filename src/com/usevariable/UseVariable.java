b83f187887bb931432e0b1f501aee6c55d37a243
new-biee <anhvtth1907004@fpt.edu.vn>
2020-11-04 16:01:48 +0700

    Add all file

---
+++ src/com/usevariable/UseVariable.java
@@ -0,0 +1,19 @@
+package com.usevariable;
+
+public class UseVariable {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+
+        System.out.println("i = " + i);
+        System.out.println("i = " + f);
+        System.out.println("i = " + d);
+        System.out.println("i = " + b);
+        System.out.println("i = " + c);
+        System.out.println("i = " + s);
+    }
+}
